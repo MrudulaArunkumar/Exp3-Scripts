9b362ec673b5b9ec900f10deea9e38b29e95fdc8
MrudulaArunkumar <mrudula.arunkumar@uni-jena.de>
2020-12-14 12:19:08 +0100

    re organizing abd created an R Project

--- Scripts/dataprepExp3.R
+++ Scripts/dataprepExp3.R
@@ -2,13 +2,17 @@
 
 library(tidyverse)
 library(plyr)
-
+#writing the directory as a variable
 dir <- setwd("D:/PhD/Experiments/Exp3 Overshadowing/Data/Exp3Overshadowing_v2")
+
+#saving all the csv files in that directory into one variable
 allOldata <- list.files(path = dir, pattern = "*.csv")
 
+#making the file names into one list
 Exp3data <- lapply(allOldata, read_csv)
 
-#ysing rbind.fill because some columns do not exist in some datafiles, namely the "preResp.key" 
+#using rbind to save it al in on df
+#using rbind.fill because some columns do not exist in some datafiles, namely the "preResp.key" 
 Exp3 <- do.call(rbind.fill,Exp3data)
 
 write.csv(Exp3, file = "Exp3_almostfulldataset.csv")
